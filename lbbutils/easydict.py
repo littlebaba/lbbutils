d851e2584c821a9704c3e0c1dfb81ed065b62a7e
littlebaba <453984016@qq.com>
2020-05-10 16:30:45 +0800

    No substantial change

--- lbbutils/easydict.py
+++ lbbutils/easydict.py
@@ -1,19 +1,22 @@
 class EasyDict(dict):
 
     def __init__(self, d=None, **kwargs):
-        if d == None:
+        if d is None:
             d = {}
         for k, v in d.items():
             setattr(self, k, v)
 
     def __setattr__(self, name, value):
+        if isinstance(value, dict):
+            value = self.__class__(value)
         super(EasyDict, self).__setattr__(name, value)
         super(EasyDict, self).__setitem__(name, value)
 
 
-
 if __name__ == '__main__':
-    d = EasyDict({'name': '李恒','age':19})
-    d.phone = '100'
-    dp=d.phone
+    d = EasyDict({'foo': 3, 'bar': {'x': 1, 'y': 2}})
+    dp = d.foo
+    x = d.bar.x
+    dp1 = d['foo']
+    x1 = d['bar']['x']
     a = 1
