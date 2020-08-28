2d7798b855f2f67e94432fb67b9159f5d03b57b7
littlebaba <453984016@qq.com>
2020-08-28 16:31:58 +0800

    do sth

--- lbbutils/file_search.py
+++ lbbutils/file_search.py
@@ -5,4 +5,17 @@
     如果一个文件中有搜索内容的话打开
     如果没有搜索内容的话关闭s
 """
+import re
 
+
+def has_key(path, key=None):
+    with open(path, 'r',encoding='utf8') as f:
+        context = f.readlines()
+    for k,c in enumerate(context):
+        match=re.search(key, c)
+        if match is not None:
+            print(f'{key} 出现在文件中第{k}行,{[col[0] for col in match.regs][0]}列')
+
+
+if __name__ == '__main__':
+    has_key("./timer.py","fnc")
