d2a3d17fc34908ee3d829a40e2bb153de82f8a70
liheng <453984016@qq.com>
2022-06-26 22:26:33 +0800

    修改Attention结构

--- lbbutils/modules/model.py
+++ lbbutils/modules/model.py
@@ -31,12 +31,12 @@ class PatchEmbed(nn.Module):
 
 
 class Attention(nn.Module):
-    def __int__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop_ratio=0., proj_drop_ratio=0.):
+    def __init__(self, dim=768, num_heads=12, qkv_bias=False, qk_scale=None, attn_drop_ratio=0., proj_drop_ratio=0.):
         '''
 
         Args:
-            dim:
-            num_heads:
+            dim: 768
+            num_heads: 12
             qkv_bias:
             qk_scale:
             attn_drop_ratio:
@@ -45,14 +45,30 @@ class Attention(nn.Module):
         Returns:
 
         '''
-        super(Attention, self).__init__()
+        super().__init__()
         self.num_heads = num_heads
-        head_dim = dim // num_heads
+        head_dim = dim // num_heads  # 整除
         self.scale = qk_scale or head_dim ** -0.5
         self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
         self.attn_drop = nn.Dropout(attn_drop_ratio)
         self.proj = nn.Linear(dim, dim)
         self.proj_drop = nn.Dropout(proj_drop_ratio)
+
+    def forward(self, x):
+        B, N, C = x.shape  # [B, 197, 768]
+        qkv_1 = self.qkv(x)  # (2,196,2304)
+        qkv_2 = qkv_1.reshape(B, N, 3, self.num_heads, C // self.num_heads)  # (2,196,3,12,64)
+        qkv = qkv_2.permute(2, 0, 3, 1, 4)  # (3,2,12,196,64)
+        # .reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)  # (B,197,12,768/12) = (B,197,12,64)
+        q, k, v = qkv[0], qkv[1], qkv[2]  # (2,12,196,64)
+        attn = (q @ k.transpose(-2, -1)) * self.scale
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)  # (2,12,196,196)
+        x = (attn @ v).transpose(1, 2)  # (2,196,12,64)
+        x = x.reshape(B, N, C)  # (2,196,768)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
 
 
 class Block(nn.Module):
@@ -64,7 +80,11 @@ class Block(nn.Module):
 
 
 if __name__ == '__main__':
+    # 测试PatchEmbed
     img = torch.ones((2, 3, 224, 224))
     model = PatchEmbed()
     y = model(img)
+    # 测试 Attention
+    att = Attention()
+    att_out = att(y)
     a = 1
