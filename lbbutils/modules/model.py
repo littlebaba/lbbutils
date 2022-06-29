2118937ac573b1e6e344bcb38cc88a737c244375
liheng <453984016@qq.com>
2022-06-29 23:22:58 +0800

    加入mlp结构

--- lbbutils/modules/model.py
+++ lbbutils/modules/model.py
@@ -71,11 +71,58 @@ class Attention(nn.Module):
         return x
 
 
+class Mlp(nn.Module):
+    def __init__(self, in_features=768, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
+        """
+
+        Args:
+            in_features:  768
+            hidden_features: 48
+            out_features:
+            act_layer:
+            drop:
+        """
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = act_layer()
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop = nn.Dropout(drop)
+
+    def forward(self,x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+        return x
+
 class Block(nn.Module):
-    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop_ratio=0., attn_drop_ratio=0.,
+    def __init__(self, dim=768, num_heads=12, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop_ratio=0., attn_drop_ratio=0.,
                  drop_path_ratio=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm):
+        """
+
+        Args:
+            dim:
+            num_heads:
+            mlp_ratio:
+            qkv_bias:
+            qk_scale:
+            drop_ratio:
+            attn_drop_ratio:
+            drop_path_ratio:
+            act_layer:
+            norm_layer:
+        """
         super(Block, self).__init__()
         self.norm1 = norm_layer(dim)
+        self.att = Attention(dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale, attn_drop_ratio=attn_drop_ratio,
+                             proj_drop_ratio=drop_ratio)
+        self.drop_path = nn.Identity()
+        self.norm2 = norm_layer(dim)
+        mlp_hidden_dim = int(dim * mlp_ratio)
+
         pass
 
 
@@ -83,7 +130,7 @@ if __name__ == '__main__':
     # 测试PatchEmbed
     img = torch.ones((2, 3, 224, 224))
     model = PatchEmbed()
-    y = model(img) #（2,196,768）
+    y = model(img)  # （2,196,768）
     # 测试 Attention
     att = Attention()
     att_out = att(y)
