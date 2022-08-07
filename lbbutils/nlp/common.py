76b4bc11ba3e6f83ba542a6fd7729c65cc1e790e
liheng <453984016@qq.com>
2022-08-07 23:38:33 +0800

    !跑通德语到英语的翻译

--- lbbutils/nlp/common.py
+++ lbbutils/nlp/common.py
@@ -183,64 +183,7 @@ def main_WMT16():
     pickle.dump(data, open('m30k_deen_shr.pkl', 'wb'))
 
 
-from torchtext.data.utils import get_tokenizer
-from typing import Iterable, List
-from torchtext.vocab import build_vocab_from_iterator
-from torchtext.datasets import Multi30k
 
-UNK_IDX, PAD_IDX, BOS_IDX, EOS_IDX = 0, 1, 2, 3
-SRC_LANGUAGE = 'de'
-TGT_LANGUAGE = 'en'
-
-
-def _main_WMT16():
-    token_transform = {}
-    vocab_transform = {}
-
-    token_transform[SRC_LANGUAGE] = get_tokenizer('spacy', language='de_core_news_sm')
-    token_transform[TGT_LANGUAGE] = get_tokenizer('spacy', language='en_core_web_sm')
-
-    def yield_tokens(data_iter: Iterable, language: str) -> List[str]:
-        language_index = {SRC_LANGUAGE: 0, TGT_LANGUAGE: 1}
-        for data_sample in data_iter:
-            yield token_transform[language](data_sample[language_index[language]])
-
-    special_symbols = ['<unk>', '<pad>', '<bos>', '<eos>']
-    for ln in [SRC_LANGUAGE, TGT_LANGUAGE]:
-        train_iter = Multi30k(split='train', language_pair=(SRC_LANGUAGE, TGT_LANGUAGE))
-        vocab_transform[ln] = build_vocab_from_iterator(yield_tokens(train_iter, ln),
-                                                        min_freq=1,
-                                                        specials=special_symbols,
-                                                        special_first=True)
-    for ln in [SRC_LANGUAGE, TGT_LANGUAGE]:
-        vocab_transform[ln].set_default_index(UNK_IDX)
-
-    from torch.nn.utils.rnn import pad_sequence
-    import torch
-
-    def sequential_transforms(*transforms):
-        def func(txt_input):
-            for transform in transforms:
-                txt_input = transform(txt_input)
-            return txt_input
-
-        return func
-
-    def tensor_transform(token_ids: List[int]):
-        return torch.cat((torch.tensor([BOS_IDX]), torch.tensor(token_ids), torch.tensor([EOS_IDX])))
-
-    text_transform = {}
-    for ln in [SRC_LANGUAGE, TGT_LANGUAGE]:
-        text_transform[ln] = sequential_transforms(token_transform[ln], vocab_transform[ln], tensor_transform)
-
-    def collate_fn(batch):
-        src_batch, tgt_batch = [], []
-        for src_sample, tgt_sample in batch:
-            src_batch.append(text_transform[SRC_LANGUAGE](src_sample.rstrip("\n")))
-            tgt_batch.append(text_transform[TGT_LANGUAGE](tgt_sample.rstrip("\n")))
-        src_batch = pad_sequence(src_batch, padding_value=PAD_IDX)
-        tgt_batch = pad_sequence(tgt_batch, padding_value=PAD_IDX)
-        return src_batch, tgt_batch
 
 
 if __name__ == '__main__':
